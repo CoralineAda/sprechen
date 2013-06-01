589a188dabe1860b7769f99c95cd54a173bc15cf
Bantik <corey@idolhands.com>
2013-06-01 09:04:39 -0500

    Skeletal Rails app.

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'bourbon'
+gem 'bson_ext'
+gem 'jquery-rails'
+gem 'mongoid'
+gem 'mongo_ext'
+gem 'neat'
+gem 'rails', '3.2.13'
+
+group :development, :test do
+  gem 'rspec-rails'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sprechen::Application.config.secret_token = '9f88c72d190caa6ce3a2d433feed96ab03a836e88b8ae382e18305cdf4ad6b26beb38472b3603079b107bad9849d7b525aab2666ac2e4246e371f4058aff2716'
